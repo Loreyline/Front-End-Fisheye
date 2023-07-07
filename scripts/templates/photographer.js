2a3b2728bffe5ed83239584fcc1e61884d25ac58
Karine Moutier <karine0805@live.fr>
2023-07-07 11:08:51 +0200

    ajout fonction fetch + images

--- scripts/templates/photographer.js
+++ scripts/templates/photographer.js
@@ -1,14 +1,16 @@
 function photographerTemplate(data) {
     const { name, portrait } = data;
 
-    const picture = `assets/photographers/${portrait}`;
+    const picture = `assets/samplePhotos/Photographers_ID_Photos/${portrait}`;
 
     function getUserCardDOM() {
-        const article = document.createElement( 'article' );
-        const img = document.createElement( 'img' );
+        const article = document.createElement('article');
+        const img = document.createElement('img');
         img.setAttribute("src", picture)
-        const h2 = document.createElement( 'h2' );
+        img.setAttribute("alt", name)
+        const h2 = document.createElement('h2');
         h2.textContent = name;
+        h2.setAttribute("aria-label", name)
         article.appendChild(img);
         article.appendChild(h2);
         return (article);
