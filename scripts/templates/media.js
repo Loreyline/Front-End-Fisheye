b43b03f8d018ed8821b4fc410f8129381854f7d1
Karine Moutier <karine0805@live.fr>
2023-08-03 06:47:26 +0200

    gestion des likes

--- scripts/templates/media.js
+++ scripts/templates/media.js
@@ -1,41 +1,100 @@
 function pictureTemplate(data) {
 
-    const { id, photographerId, title, image, likes, date, price } = data;
+    const { id, photographerId, title, image, video, likes, date, price } = data;
 
     const picture = `assets/samplePhotos/${photographerId}/${image}`;
+    const mediaVideo = `assets/samplePhotos/${photographerId}/${video}`;
 
     function getPicturesDom() {
 
         //création des éléments du dom
         const article = document.createElement('article');
-        const section = document.createElement('section')
+        const section = document.createElement('section');
+        const video = document.createElement('video');
+        const source = document.createElement('source');
         const img = document.createElement('img');
         const h3 = document.createElement('h3');
-        const p = document.createElement('p');
-        const icone = document.createElement('i');
+        const pLike = document.createElement('p');
+        const sLikes = document.createElement('span');
+        const likeButton = document.createElement('i');
+        const pVideo = document.createElement('p');
+        const lienLightBox = document.createElement('a');
+        const lienVideo = document.createElement('a');
+
+        //gestion des likes
+        data.isLiked = false;
+        let totalLikes = 0;
+        const nbLikes = parseInt(likes)
+        totalLikes += nbLikes;
+
+        if (data.isLiked) {
+            likeButton.setAttribute("class", "fa-solid fa-heart");
+        } else {
+            likeButton.setAttribute("class", "fa-regular fa-heart");
+        }
+
+        sLikes.textContent = totalLikes + " ";
+
+        likeButton.addEventListener('click', function () {
+
+            if (data.isLiked) {
+                totalLikes--;
+                data.isLiked = false;
+                likeButton.setAttribute("class", "fa-regular fa-heart");
+            } else {
+                totalLikes++;
+                data.isLiked = true;
+                likeButton.setAttribute("class", "fa-solid fa-heart");
+            }
+
+            sLikes.textContent = totalLikes + " ";
+        });
 
         //insertion des attributs des éléments
+        source.setAttribute("src", mediaVideo);
+        video.setAttribute("controls", true);
+        source.setAttribute("type", "video/mp4");
+        video.setAttribute("aria-role", "informations video");
         img.setAttribute("src", picture);
         img.setAttribute("alt", title);
         img.setAttribute("aria-role", "informations image");
         h3.setAttribute("aria-label", title);
-        p.setAttribute("aria-label", likes + "likes");
-        icone.setAttribute("class", "fa-solid fa-heart");
-        icone.setAttribute("title", "likes");
+        pLike.setAttribute("aria-label", sLikes.textContent + "likes");
+        pLike.setAttribute("id", id);
+        likeButton.setAttribute('role', 'button');
+        likeButton.setAttribute('aria-label', 'Like');
+        likeButton.setAttribute("title", "likes");
+        lienLightBox.setAttribute("href", "#");
+        lienVideo.setAttribute("href", mediaVideo);
+        sLikes.setAttribute('role', 'text');
 
         //indication des textes à afficher
         h3.textContent = title;
-        p.textContent = likes + " ";
+        pVideo.textContent = "Votre navigateur ne prend pas en charge les vidéos. Voici, à la place, un ";
+        lienVideo.textContent = "lien vers la vidéo";
 
         //affichage des éléments du dom en fonction de l'emplacement choisi
-        article.appendChild(img);
+        if (data.image) {
+            lienLightBox.appendChild(img);
+        } else {
+            lienLightBox.appendChild(video);
+            video.appendChild(source);
+            video.appendChild(pVideo);
+            pVideo.appendChild(lienVideo);
+        }
+
+        article.appendChild(lienLightBox);
         article.appendChild(section);
         section.appendChild(h3);
-        section.appendChild(p);
-        p.appendChild(icone);
+        section.appendChild(pLike);
+        pLike.appendChild(sLikes);
+        pLike.appendChild(likeButton);
 
         return (article);
     }
-    return { id, photographerId, title, image, likes, date, price, getPicturesDom }
+    return { id, photographerId, title, image, video, likes, date, price, getPicturesDom }
 
 }
+
+
+
